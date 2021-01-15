d7fb205c06c9eb378a4c569d740b56988fd8c8e0
JoaoOtavioS <contato@joaootavios.com.br>
2021-01-15 03:17:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // developed by JoaoOtavioS.
 // Version: 1.0 - last update: 29/12/2020
-const discord = require('discord.js'), credenciais = require('./database/credentials.json'), config = require('./database/config.json'), color = require("tynt"), fs = require('fs'), moment = require('moment')
+const discord = require('discord.js'), credenciais = require('./database/credentials.json'), config = require('./database/config.json'), fs = require('fs');
 const client = new discord.Client({ messageCacheMaxSize: 200, messageCacheLifetime: 604800, messageSweepInterval: 604800, disableEveryone: true });
-
-if (!fs.existsSync('./languages/language_' + config.language + '.json')) { console.log(color.Red("\nLanguage config's is wrong, bot has been stopped.\n")), client.destroy(); return; } else { console.log(color.Green("Language config's correctly.\n")) }
-var lang = require('./languages/language_' + config.language + '.json'), error = lang.error_notfound;
-moment().format(), moment.locale(config["moment-lang"])
 
 // Limitations:
 // Support only guild. (to prevent errors)
@@ -14,6 +10,6 @@ client.on('ready', () => {
     console.log(color.Green("\n" + lang.starting_completely) + "\n" + color.Red(" [" + error + "]\n"))
 })
 
-require('./moduleloader.js')(client, discord, config, credenciais, lang, color, fs)
+require('./moduleloader.js')(client, discord, config, credenciais)
 client.commands = new discord.Collection(), client.aliases = new discord.Collection()
 client.login(credenciais.token)
